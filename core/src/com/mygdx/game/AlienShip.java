39fd535f3f87f23c89568fe4700b21365eca0dcd
baker203 <baker203@iupui.edu>
2017-05-24 01:19:44 -0400

    Changed x,y position variables to position vectors

--- core/src/com/mygdx/game/AlienShip.java
+++ core/src/com/mygdx/game/AlienShip.java
@@ -1,90 +1,54 @@
 package com.mygdx.game;
 
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.Batch;
-import com.badlogic.gdx.graphics.g2d.Sprite;
-import com.badlogic.gdx.math.Vector2;
+import com.mygdx.game.Weapons.MachineGun;
 
 /**
  * Created by Joshua on 5/21/2017.
  */
 
-public class AlienShip extends Enemy {
+public class AlienShip extends Ship{
 
-    private final String imagePath = "enemyship1.png";
-
-    //private final int screenWidth;
-    //private final int screenHeight;
-
-    private final float WORLD_WIDTH;
-    private final float WORLD_HEIGHT;
+    private static final String imagePath = "enemyship1.png";
+    private static final int startingHealth = 1;
 
     private float speed;
     private float xDir;
 
     public AlienShip(float WORLD_WIDTH, float WORLD_HEIGHT){
-
-        this.WORLD_HEIGHT = WORLD_HEIGHT;
-        this.WORLD_WIDTH = WORLD_WIDTH;
-
+        super(WORLD_WIDTH, WORLD_HEIGHT, imagePath, new MachineGun(), startingHealth);
+        pos.set(0, WORLD_HEIGHT - getHeight());
         speed = 50;
         xDir = 1;
 
-        health = 1;
-
-        sprite = new Sprite(new Texture(imagePath));
-
-        sprite.setPosition(0,WORLD_HEIGHT - sprite.getHeight());
-
-        //sprite.setPosition(WORLD_WIDTH/2 - sprite.getWidth(), WORLD_HEIGHT - 10);
     }
 
     public void move(float delta){
 
-        float nextPos = xDir * speed * delta + sprite.getX();
+        float nextPos = xDir * speed * delta + getX();
 
         if (nextPos <= 0){
             nextPos = 0;
             changeXDir();
         }
 
-        if (nextPos + sprite.getWidth() >= WORLD_WIDTH){
-            nextPos = WORLD_WIDTH - sprite.getWidth();
+        if (nextPos + getWidth() >= WORLD_WIDTH){
+            nextPos = WORLD_WIDTH - getWidth();
             changeXDir();
         }
 
-        sprite.setX(nextPos);
+        setX(nextPos);
 
     }
 
     public void changeXDir(){
+
         xDir *= -1;
-    }
-
-
-    public void draw(Batch batch){
-
-        sprite.draw(batch);
 
     }
 
-    public void setX(int x){
-        sprite.setX(x);
+    @Override
+    public void hit(float damage) {
+        //TODO
     }
 
-    public void setY(int y){
-        sprite.setY(y);
-    }
-
-    public float getLeft(){ return sprite.getX(); }
-
-    public float getRight(){ return sprite.getX() + sprite.getWidth(); }
-
-    public float getX(){
-        return sprite.getX();
-    }
-
-    public float getY(){
-        return sprite.getY();
-    }
 }
